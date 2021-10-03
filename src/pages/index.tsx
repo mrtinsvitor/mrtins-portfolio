ff56dc468ccfb63b6e02ebc53aa05ddfb4bc7c09
Vitor Martins <vitor.martins@stef.com>
2021-10-03 19:25:02 +0100

    Adding meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,18 @@ const Home: NextPage = () => {
 		<div>
 			<Head>
 				<title>Vitor Martins</title>
-				<meta name="description" content="Software Engineer" />
+				<meta
+					name="description"
+					content="Hey I'm Vitor, and I build things. I'm experienced in Fullstack Web
+						Development but I also like developing mobile applications
+						(hybrid/native). There are some technologies that I'm used to
+						working on a daily basis such as JavaScript and frameworks like
+						React.Js, Angular, Node.Js, and also have good experience with Java
+						and the Spring Framework. And as a Fullstack developer I also got
+						some experiences with SQL and NoSQL databases, AWS Services and
+						Docker."
+				/>
+				<meta name="viewport" content="width=device-width,initial-scale=1.0" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
