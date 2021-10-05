7e5bcc669865a2396bc093751497f7e67b82f734
Vitor Martins <vitor.martins@stef.com>
2021-10-05 19:53:37 +0100

    Removing unused components

--- src/components/shared/Button/Button.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Button from './index';
-
-describe('Button', () => {
-	it('renders without crashing', () => {
-		render(<Button />);
-	});
-});
--- src/pages/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Home from './index';
-
-describe('Home', () => {
-	it('renders without crashing', () => {
-		render(<Home />);
-	});
-});
