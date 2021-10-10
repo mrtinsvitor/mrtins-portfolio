41701329565eceeb9d88cd4fd60e327a0607b131
Vitor Martins <vitor.martins@stef.com>
2021-10-10 16:46:42 +0100

    Adding google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript
+	DocumentContext,
+	Head,
+	Html,
+	Main,
+	NextScript,
 } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
@@ -45,6 +45,22 @@ class MyDocument extends Document {
 					<link
 						href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&family=Raleway:wght@200;300;400;500;700&family=Roboto:wght@300;400;500;700&display=swap"
 						rel="stylesheet"
+					/>
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+						}}
 					/>
 				</Head>
 				<body>
